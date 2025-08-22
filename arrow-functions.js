ac08c2f93db471133287878b0450af9b0cebc3cc
Megha619801-creator <mahatomahi2062@gmail.com>
2025-08-22 16:01:25 +0300

    upload week1

---
+++ arrow-functions.js
@@ -0,0 +1,12 @@
+
+const sayHelloArrow = () => "Hello, world!";
+const doubleArrow = x => x * 2;
+const addArrow = (x, y) => x + y;
+const personArrow = {
+    name: "Alice",
+    sayHi: () => "Hi, " + this.name + "!" // 'this' will not work as expected here
+};
+const numbers = [1, 2, 3, 4, 5];
+const doubled = [];
+
+numbers.forEach(num =>  doubled.push(num * 2));
---
+++ hoisting.js
@@ -0,0 +1,19 @@
+// 1.
+function cube(x) {
+  return x * x * x;
+}
+
+const cube = function(x){
+     return x*x*x;
+}
+   
+
+// 2.
+function fullName(first, last) {
+  return first + " " + last;
+}
+
+const fullName = function(first, last){
+    return first + " " + last;
+}
+
